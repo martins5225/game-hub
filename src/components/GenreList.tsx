7b19971fbbbcb0b43c3fc94248363a86278d580c
Victor Martins <martinsvictor287@gmail.com>
2023-05-23 01:11:16 +0300

    Implement Spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,10 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenre';
 
 const GenreList = () => {
-	const { data } = useGenres();
+	const { data, isLoading } = useGenres();
+
+	if (isLoading) return <Spinner />;
 	return (
 		<List>
 			{data.map((genre) => (
