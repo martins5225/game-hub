be5dbb5ce991b92cb1da3c4517945c7ff6348ce5
Victor Martins <martinsvictor287@gmail.com>
2023-10-13 09:10:11 +0300

    implement infinite queries

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,42 +1,49 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Button, SimpleGrid, Text } from '@chakra-ui/react';
 import useGames, { Game } from '../hooks/useGames';
 
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
 import { GameQuery } from '../App';
+import React from 'react';
 
 interface Props {
-	gameQuery: GameQuery;
+  gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-	const { data, error, isLoading } = useGames(gameQuery);
-	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  const { data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = useGames(gameQuery);
 
-	return (
-		<>
-			{error && <Text>{error.message}</Text>}
+  const renderSkeletons = Array.from({ length: 16 }).map((_, index) => (
+    <GameCardContainer key={index}>
+      <GameCardSkeleton />
+    </GameCardContainer>
+  ));
 
-			<SimpleGrid
-				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-				padding={10}
-				spacing={8}
-			>
-				{isLoading &&
-					skeletons.map((Skeleton) => (
-						<GameCardContainer key={Skeleton}>
-							<GameCardSkeleton />
-						</GameCardContainer>
-					))}
-				{data?.results.map((game: Game) => (
-					<GameCardContainer key={game.id}>
-						<GameCard game={game} />
-					</GameCardContainer>
-				))}
-			</SimpleGrid>
-		</>
-	);
+  const renderGames = data?.pages.map((page, index) => (
+    <React.Fragment key={index}>
+      {page.results.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </React.Fragment>
+  ));
+
+  return (
+    <Box padding="10px">
+      {error && <Text>{error.message}</Text>}
+
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}  spacing={8}>
+        {isLoading && renderSkeletons}
+        {renderGames}
+      </SimpleGrid>
+
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()}>{isFetchingNextPage ? 'Loading...' : 'Load More'}</Button>
+      )}
+    </Box>
+  );
 };
 
 export default GameGrid;
